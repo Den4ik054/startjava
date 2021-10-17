3cf2aeafc552bd6b26623fd69dda2f963a888eb6
Denis Chernov <denis1983chernov@gmail.com>
2021-10-17 16:54:26 +0700

    Less_1_DZ

---
+++ Lesson 1/Variable.java
@@ -0,0 +1,24 @@
+import java.lang.String;
+
+public class Variable {
+    public static void main(String[] args) {
+    
+    byte cp = 4;
+    short ram = 8;
+    int hdd = 2;
+    long vga = 2;
+    float audio = 3.5f;
+    double wi_fi = 802.11d;
+    char ssd = '1';
+    boolean fan = true;
+
+    System.out.println(cp);
+    System.out.println(ram);
+    System.out.println(hdd);
+    System.out.println(vga);
+    System.out.println(audio);
+    System.out.println(wi_fi);
+    System.out.println(ssd);
+    System.out.print(fan);
+    }
+}
