250366e9ce41c89a498bedf1f4e0913902728129
Denis Chernov <denis1983chernov@gmail.com>
2021-10-18 21:08:21 +0700

    final

--- Lesson 1/ConditionalStatement.java
+++ Lesson 1/ConditionalStatement.java
@@ -1,6 +1,5 @@
 public class ConditionalStatement {
     public static void main(String[] args) {
-
         int age = 25;
         if(age > 20) {
             System.out.println("Продажа разрешена");
