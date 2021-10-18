009009e505d4f8f2f9607d7ec2e296f68eeb9f2f
Denis Chernov <denis1983chernov@gmail.com>
2021-10-18 19:44:15 +0700

    fix

--- Lesson 1/ConditionalStatement.java
+++ Lesson 1/ConditionalStatement.java
@@ -1,27 +1,31 @@
 public class ConditionalStatement {
     public static void main(String[] args) {
-        int age = 25;
-        boolean isMale = true;
-        float height = 1.6f;
-        char latname = 'I';
 
+        int age = 25;
         if(age > 20) {
             System.out.println("Продажа разрешена");
         }
+
+        boolean isMale = true;
         if(isMale) {
             System.out.println("Мужчина");
         }
+
         if(!isMale) {
             System.out.println("Не мужчина");
         }
+
+        float height = 1.6f;
         if(height < 1.80) {
             System.out.println("Ваш рост " + height);
         } else {
             System.out.println("играй в воллейбол");
         }
-        if(latname == 'M') {
+
+        char firstLetterName = 'I';
+        if(firstLetterName == 'M') {
             System.out.println("имя начинается на М?");
-        } else if(latname == 'I') {
+        } else if(firstLetterName == 'I') {
             System.out.println("имя начинается на I?");
         } else {
             System.out.println("Как Вас зовут ?");
