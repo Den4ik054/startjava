43523d2abcf8693147d4323551e4232898fb8b72
Denis Chernov <denis1983chernov@gmail.com>
2021-10-21 20:10:34 +0700

    Ver_1.0

---
+++ Lesson 1/Calculator.java
@@ -0,0 +1,41 @@
+// Реализуйте Калькулятор, который должен уметь выполнять математические операции (+, -, *, /, ^, %) над целыми положительными числами:
+// что бы программа могла выполнить ту или иную математическую операцию (одну, а не все сразу), делайте проверку математического знака,
+//используя if else
+// выведите на экран результат вычисления с помощью System.out.println()
+// Символ ^ используется в данном задании для обозначения операции возведения в степень, но в Java он выполняет совсем иную функцию. 
+//использовать его для возведения в степень не получится. Самостоятельно реализуйте данную операцию (не юзайте Math.pow())
+// Символ %, похожий на проценты, называется оператором деления по модулю. В калькуляторе не надо считать проценты! 
+// Считайте остаток от деления
+public class Calculator {
+    public static void main(String[] args) {
+        int firstNumber = 3;
+        System.out.println("Введите первое число " + firstNumber);
+
+        int secondNumber = 4;
+        System.out.println("Введите второе число " + secondNumber);
+
+        char operation = '/';
+        System.out.println("Введите необходимую операцию " + operation);
+
+        int temp = firstNumber;
+        if(operation == '^' && (firstNumber > 0 && secondNumber > 0)) {
+        for (int i = 1; i < secondNumber; i++) {
+            temp = temp * firstNumber;
+        }
+            System.out.println("Результат = " + temp);
+        } else if(operation == '%' && (firstNumber > 0 && secondNumber > 0)) {
+            System.out.println("Результат = " + (firstNumber % secondNumber));
+        } else if(operation == '+' && (firstNumber >= 0 && secondNumber >= 0)) {
+            System.out.println("Результат = " + (firstNumber + secondNumber));
+        } else if(operation == '*' && (firstNumber >= 0 && secondNumber >= 0)) {
+            System.out.println("Результат = " + (firstNumber * secondNumber));
+        } else if(operation == '/' &&  secondNumber == 0) {
+            System.out.println("На ноль делить нельзя");
+        } else if(operation == '/' && (firstNumber > 0 && secondNumber > 0)) {
+            System.out.println("Результат = " + (firstNumber / secondNumber));
+        } else if(operation == '-' && (firstNumber > 0 && secondNumber > 0)) {
+            System.out.println("Результат = " + (firstNumber - secondNumber));
+            }
+        
+    }
+}
