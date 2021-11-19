fcc9379633c3156f5e3f825344cdb5d7f42487f0
Denis Chernov <denis1983chernov@gmail.com>
2021-11-19 14:05:53 +0700

    fix_dz_6

--- Lesson 2/Calculator.java
+++ Lesson 2/Calculator.java
@@ -1,66 +1,52 @@
-// класс Calculator будет содержать код, отвечающий за проверку математического знака и сами вычисления
-// замените все if else, отвечающие за проверку математического знака, на оператор switch
-// для ввода математического выражения используйте клавиатуру и класс Scanner
-// формат ввода математического выражения:
-// Введите первое число: 2
-// Введите знак математической операции: ^
-// Введите второе число: 10
+/*класс Calculator будет содержать код, отвечающий за проверку математического знака и сами вычисления
+замените все if else, отвечающие за проверку математического знака, на оператор switch
+для ввода математического выражения используйте клавиатуру и класс Scanner
+формат ввода математического выражения:
+Введите первое число: 2
+Введите знак математической операции: ^
+Введите второе число: 10*/
 public class Calculator {
-    
+
     private int firstNumber;
     private int secondNumber;
     private char operation;
 
-    public int getFirstNumber() {
-        return firstNumber;
-    }
-
     void setFirstNumber(int firstNumber) {
         this.firstNumber = firstNumber;
-    }
-
-    public int getSecondNumber() {
-        return secondNumber;
     }
 
     void setSecondNumber(int secondNumber) {
         this.secondNumber = secondNumber;
     }
 
-    public char getOperation() {
-        return operation;
-    }
-
     void setOperation(char operation) {
         this.operation = operation;
     }
 
-    public void calculate () {
+    public void calculate() {
         int degree = firstNumber;
-        for(int i = 1; i < secondNumber; i++) {
-            switch(operation) {
-                case '^' :
-                    degree *= firstNumber;
-                    break;
-        }
-            }
-                System.out.println("Результат = " + degree);
         switch(operation) {
-            case '+' :
-                System.out.println("Результат = " + (firstNumber + secondNumber));
-                break;
-            case '-' :
-                System.out.println("Результат = " + (firstNumber - secondNumber));
-                break;
-            case '*' :
-                System.out.println("Результат = " + (firstNumber * secondNumber));
-                break;
-            case '/' :
-                System.out.println("Результат = " + (firstNumber / secondNumber));
-                break;
-            case '%' :
-                System.out.println("Результат = " + (firstNumber % secondNumber));
-                break;
+        case '^' :
+            for(int i = 1; i < secondNumber; i++) {
+                degree *= firstNumber;
+            }
+            System.out.println("Результат = " + degree);
+            break;
+        case '+' :
+            System.out.println("Результат = " + (firstNumber + secondNumber));
+            break;
+        case '-' :
+            System.out.println("Результат = " + (firstNumber - secondNumber));
+            break;
+        case '*' :
+            System.out.println("Результат = " + (firstNumber * secondNumber));
+            break;
+        case '/' :
+            System.out.println("Результат = " + (firstNumber / secondNumber));
+            break;
+        case '%' :
+            System.out.println("Результат = " + (firstNumber % secondNumber));
+            break;
         }
     }
 }
--- Lesson 2/CalculatorTest.java
+++ Lesson 2/CalculatorTest.java
@@ -1,34 +1,34 @@
-// создайте класс CalculatorTest, который будет содержать метод main 
-// и отвечать за создание объектов, их инициализацию, ввод математического выражения и запуск вычислений
-// по окончанию каждого вычисления выводите сообщение "Хотите продолжить вычисления? [yes/no]:" (реализуйте это в CalculatorTest):
-// yes — пользователю заново предлагается ввести математическое выражение
-// no — программа завершается
-// при вводе иного значения выводите сообщение до тех пор, пока не будут введены допустимые ответы
+/*создайте класс CalculatorTest, который будет содержать метод main 
+и отвечать за создание объектов, их инициализацию, ввод математического выражения и запуск вычислений
+по окончанию каждого вычисления выводите сообщение "Хотите продолжить вычисления? [yes/no]:" (реализуйте это в CalculatorTest):
+yes — пользователю заново предлагается ввести математическое выражение
+no — программа завершается
+при вводе иного значения выводите сообщение до тех пор, пока не будут введены допустимые ответы*/
 import java.util.Scanner;
+
 public class CalculatorTest {
     public static void main(String[] args) {
         String inputValidation = "yes";
 
+        Scanner console = new Scanner(System.in);
         while(inputValidation.equals("yes")) {
-            Calculator calcOne = new Calculator();
-            Scanner console = new Scanner(System.in);
+            Calculator calc = new Calculator();
+            
             System.out.println("Введите первое число ");
-            calcOne.setFirstNumber(console.nextInt());
+            calc.setFirstNumber(console.nextInt());
 
             System.out.println("Введите знак математической операции: ");
-            calcOne.setOperation(console.next().charAt(0));
+            calc.setOperation(console.next().charAt(0));
 
             System.out.println("Введите второе число: ");
-            calcOne.setSecondNumber(console.nextInt());
+            calc.setSecondNumber(console.nextInt());
 
-            calcOne.calculate ();
+            calc.calculate();
 
-            inputValidation = "";
-            while (!inputValidation.equals("yes") && !inputValidation.equals("no")) {
-                Scanner scanner = new Scanner(System.in);
-                System.out.println("Хотите продолжить вычисления? [yes/no]:");
-                inputValidation = scanner.nextLine();
-            }
+            System.out.println("Хотите продолжить вычисления? [yes/no]:");
+            do {
+                inputValidation = console.nextLine();
+            } while (!inputValidation.equals("yes") && !inputValidation.equals("no"));
         }
     }
 }
